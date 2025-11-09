df01670ac661427cec8549603adc37d7e2729c65
Alireza Ezlegini <alireza.ezlegini@yahoo.com>
2025-11-09 16:54:49 +0330

    Bring back security to admin

--- apps/admin/app/(DASHBOARD)/layout.tsx
+++ apps/admin/app/(DASHBOARD)/layout.tsx
@@ -4,13 +4,14 @@ import {
   SidebarProvider,
 } from "@parallane/ui/components/ui/sidebar";
 import DashboardSidebar from "../../components/sidebar/Dashboard-Sidebar";
+import { authenticateSession } from "@/lib/auth";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // await authenticateSession();
+  await authenticateSession();
 
   return (
     <div className="">
