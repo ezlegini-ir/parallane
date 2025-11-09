3504bc6f567e8de7efd35c5c60ca967d89687dcd
Alireza Ezlegini <alireza.ezlegini@yahoo.com>
2025-11-09 15:25:31 +0330

    chore: comment out session authentication and redirect logic in layout and middleware

--- apps/admin/app/(DASHBOARD)/layout.tsx
+++ apps/admin/app/(DASHBOARD)/layout.tsx
@@ -11,7 +11,7 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  await authenticateSession();
+  // await authenticateSession();
 
   return (
     <div className="">
